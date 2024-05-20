93a40c80293cdde43bce86afd9666e3f954a57d5
David <Dgagiev@gmail.com>
2024-05-20 12:31:46 +0300

    Toolbar вторая часть добавили bottom меню

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,26 +6,42 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Admin"
+        //binding.bNav.selectedItemId = R.id.ic_del
+        binding.bNav.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                // android.R.id.home -> finish()
+                R.id.ic_save -> {
+                    Toast.makeText(this, "Del", Toast.LENGTH_SHORT).show()
+                }
+                R.id.ic_sync -> {
+                    Toast.makeText(this, "Sync", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
+        menuInflater.inflate(R.menu.bottom_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             // android.R.id.home -> finish()
-            R.id.ic_sync -> Toast.makeText(this, "Sync", Toast.LENGTH_SHORT).show()
 
         }
         return true
