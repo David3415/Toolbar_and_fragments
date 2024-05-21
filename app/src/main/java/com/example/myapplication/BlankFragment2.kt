aaeba7c4c016ced73e0699032ac7a5a7ebc04794
David <Dgagiev@gmail.com>
2024-05-21 11:53:55 +0300

    Fragment 1.1

--- app/src/main/java/com/example/myapplication/BlankFragment2.kt
+++ app/src/main/java/com/example/myapplication/BlankFragment2.kt
@@ -6,22 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class BlankFragment : Fragment() {
-   // lateinit var binding:
+
+class BlankFragment2 : Fragment() {
+    // TODO: Rename and change types of parameters
+       override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return inflater.inflate(R.layout.fragment_blank2, container, false)
     }
 
-    companion object {
-        fun newInstance() =
-            BlankFragment().apply {
-                arguments = Bundle().apply {
 
-                }
-            }
-    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,15 +6,17 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Admin"
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction().replace(binding.placeHolder.id,BlankFragment.newInstance()).commit()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -30,5 +32,6 @@ class MainActivity : AppCompatActivity() {
         }
         return true
     }
+
 
 }
