0d30bd42a926ffbb5eb677d2416e7d93bf733061
David <Dgagiev@gmail.com>
2024-05-21 14:34:11 +0300

    Fragment Урок 32 Два фрагмента, переключение по кнопке

--- app/src/main/java/com/example/myapplication/BlankFragment2.kt
+++ app/src/main/java/com/example/myapplication/BlankFragment2.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentBlank2Binding
 
 
 class BlankFragment2 : Fragment() {
@@ -14,7 +15,9 @@ class BlankFragment2 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank2, container, false)
+        val bindding=FragmentBlank2Binding.inflate(inflater)
+        return  bindding.root
+       // return inflater.inflate(R.layout.fragment_blank2, container, false)
     }
 
     companion object {
