06edfabf2ab8c91aed7297f2a7ebafbeedfe134e
David <Dgagiev@gmail.com>
2024-07-14 23:01:01 +0300

    Work version. Added switch of actionbar title when switch fragments

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -24,6 +27,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.ab.toolbar)
+        var K = findViewById<View>(R.id.actionbar1)
+        //supportActionBar?.setTitle(R.string.back_to_main_frag)
+
+                //binding.ab.toolbar?.
+            //getSupportActionBar()?.setTitle("My title")
         navController = findNavController(R.id.fragmentContainerView)
         conf = AppBarConfiguration(
             setOf(
@@ -31,11 +39,31 @@ class MainActivity : AppCompatActivity() {
                 R.id.item2
             ), binding.drawerLayout
         )
+
         setupActionBarWithNavController(navController, conf)
         binding.navView.setupWithNavController(navController)
     }
 
+    var aa = "dfs"
     override fun onSupportNavigateUp(): Boolean {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.item1 -> {
+                    val resource = getString(R.string.frag_1)
+                    Log.d("MyLog", "res: $resource")
+                    title = resource
+                }
+
+                R.id.item2 -> {
+                    val resource = getString(R.string.frag_2)
+                    Log.d("MyLog", "res: $resource")
+                    title = resource
+                }
+
+                else -> "onSupportNavigateUp ERROR"
+            }
+        }
+
         return navController.navigateUp(conf) || super.onSupportNavigateUp()
     }
 }
